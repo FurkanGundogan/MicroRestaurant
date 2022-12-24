549db0277ba4d4d62f08092122762b6722bc562e
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-12-24 12:54:22 +0300

    Product API Controller implemented.

---
+++ Mango.Web/SD.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Web
+{
+    public class SD
+    {
+        // Static Details
+        public string ProductAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
