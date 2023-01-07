aa1c1cc9302faa483ec83abd084b4d6d39c9eaf2
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2023-01-07 12:36:51 +0300

    Static definitions added: IdentityResources, ApiScopes  and Clients

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,50 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System;
+
+namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public static string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>{
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile(),
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+        {
+            new ApiScope("mango","MangoServer"),
+            new ApiScope("read","Read Your Data"),
+            new ApiScope("write","Write Your Data"),
+            new ApiScope("delete","Delete Your Data"),
+        };
+        public static IEnumerable<Client> Clients = new List<Client> { 
+            new Client
+            {
+                ClientId="client",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read","write","profile"}
+            },
+            new Client
+            {
+                ClientId="mango",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris={"http://localhost:44353/signin-oidc"},
+                PostLogoutRedirectUris={"http://localhost:44353/signout-callback-oidc"},
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "mango"
+                }
+            }
+        };
+    }
+}
