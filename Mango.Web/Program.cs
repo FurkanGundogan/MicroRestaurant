12ae1f3340d216ca50df7ae23cec7fe2057f1468
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2023-01-13 19:02:01 +0300

    Auth added. Login Partial first init

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -9,14 +9,35 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IProductService, ProductService>();
 SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 
-builder.Services.AddScoped<IProductService, ProductService>();  
+builder.Services.AddScoped<IProductService, ProductService>();
+// Configure Identity Server
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
 
+}).AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+.AddOpenIdConnect("oidc", options => {
+    options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+    options.GetClaimsFromUserInfoEndpoint= true;
+    options.ClientId = "mango";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+    options.TokenValidationParameters.NameClaimType = "name";
+    options.TokenValidationParameters.RoleClaimType = "role";
+    options.Scope.Add("mango");
+    options.SaveTokens = true;
+
+});
+// Configure Identity Server
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+   
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -25,7 +46,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
