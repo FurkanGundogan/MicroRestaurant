df8249a978e1b8215f64c55a6bbe81554cd7b92c
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2023-02-18 14:20:57 +0300

    CartRepository added.

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+        public CartRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            Cart cart = _mapper.Map<Cart>(cartDto);
+            // check if product exist in db, if not create it
+
+            // check if header is null
+                // create header and details
+
+            // if header is not null
+                // check if details has same prodcut
+                    // if it has then update the count
+                // else create details
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
