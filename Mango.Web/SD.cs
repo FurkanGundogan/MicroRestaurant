ec0569e8abe199fcbdd1bcb2ed77af8b33b81b16
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2023-02-19 12:36:44 +0300

    Models of cart api are added to mango-web

--- Mango.Web/SD.cs
+++ Mango.Web/SD.cs
@@ -4,6 +4,7 @@
     {
         // Static Details
         public static string ProductAPIBase { get; set; }
+        public static string ShoppingCartAPIBase { get; set; }
         public enum ApiType
         {
             GET,
