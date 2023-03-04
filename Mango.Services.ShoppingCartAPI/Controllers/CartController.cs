31b04638416264f08ef7d17d75b6cedfd142c97b
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2023-03-04 10:32:47 +0300

    ApplyCoupon and RemoveCoupon added.

--- Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -86,5 +86,41 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             return _response;
 
         }
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.ApplyCoupon(cartDto.CartHeader.UserId,cartDto.CartHeader.CouponCode);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+
+            }
+            return _response;
+
+        }
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] string userId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveCoupon(userId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+
+            }
+            return _response;
+
+        }
     }
 }
